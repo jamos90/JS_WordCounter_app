e595bcd9d518d9c7036f781bb2f9e0eeec8be202
jamos90 <jamos_98@hotmail.com>
2018-08-06 15:39:59 +0100

    pubsub publish added to text input

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const InputVeiw = require('./views/input_view.js');
+const PubSub = require('./helpers/pub_sub.js');
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript Loaded');
 
   const inputView = new InputVeiw;
   inputView.bindEvents();
+
 });
--- src/views/input_view.js
+++ src/views/input_view.js
@@ -1,3 +1,5 @@
+const PubSub = require('../helpers/pub_sub.js');
+
 const InputText = function () {
 
 }
@@ -6,6 +8,7 @@ const InputText = function () {
     const input = document.querySelector('#text');
     input.addEventListener('input', (evt) =>{
       const inputtedText = evt.target.value;
+      PubSub.publish("InputVeiw:text-inputted", inputtedText);
     });
 
   }
